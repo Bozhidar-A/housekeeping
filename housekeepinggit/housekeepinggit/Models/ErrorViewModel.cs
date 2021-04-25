1eecda07a96c698177e53b6c04e572632acfa2cb
bob <bplatanasov@gmail.com>
2021-04-25 10:10:02 +0300

    base asp with identity

---
+++ housekeepinggit/housekeepinggit/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace housekeepinggit.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
